9c1ccd4538f911842e307a1b776370df454623be
Maxime Njo <123020591+MaximNjo@users.noreply.github.com>
2023-02-16 23:29:57 +0100

    4

--- Service.cs
+++ Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Projet_Commercial;
+using System;
 
 public class Service
 {
@@ -7,10 +8,33 @@ public class Service
     private int effectif;
     int[] tableau = new int[10];
 
+
+
     public string NomService { get => nomService; set => nomService = value; }
     public int Effectif { get => effectif; set => effectif = value; }
     public int[] Tableau { get => tableau; set => tableau = value; }
 
 
 
+      
+      
+
+        public string CommercialAge()
+        {
+            Commercial commercialLePlusAge = null;
+            int ageMax = int.MinValue;
+            foreach (Commercial commercial in Commercial)
+            {
+                if (commercial != null && commercial.AnneeNaissance > ageMax)
+                {
+                    commercialLePlusAge = commercial;
+                    ageMax = commercial.AnneeNaissance;
+                }
+            }
+            return commercialLePlusAge;
+        }
+ 
+
+
 }
+
