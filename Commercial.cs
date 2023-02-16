8cda661b5a4bee2037be3b16de4b20103124b842
«MaximeNjo <maximenjo26@outlook.com>
2023-02-16 09:01:39 +0100

    Ajoutez des fichiers projet.

---
+++ Commercial.cs
@@ -0,0 +1,76 @@
+﻿// NJO BOJONGO MAXIME 
+
+using System;
+namespace Projet_Commercial
+{
+    public class Commercial
+    {
+        private string nom;
+        private string prenom;
+        private int anneeNaissance;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int AnneeNaissance { get => anneeNaissance; set => anneeNaissance = value; }
+
+
+    
+        public Commercial(string nom, string prenom, int anneeNaissance)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.AnneeNaissance = anneeNaissance;
+        }
+   
+
+        public int CalculAge()
+        {
+
+            int difference = DateTime.Now.Year - this.AnneeNaissance;
+            return difference;
+
+        }
+
+
+        public override string ToString()
+        {
+
+
+
+
+        }
+      
+        public string Compare(Commercial c)
+        {
+
+            Commercial commercial1 = new Commercial();
+
+
+        }
+        class Categorie
+        {
+
+            private string codeCategorie;
+            private string libCategorie;
+
+            public string CodeCategorie { get => codeCategorie; set => codeCategorie = value; }
+            public string LibCategorie { get => libCategorie; set => libCategorie = value; }
+
+            public Categorie(string CodeCategorie, string LibCategorie)
+            {
+
+                this.codeCategorie = CodeCategorie;
+                this.LibCategorie = LibCategorie;
+
+            }
+
+            public override string ToString()
+            {
+
+                return CodeCategorie + " " + LibCategorie;
+
+
+            }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Projet_Commercial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
